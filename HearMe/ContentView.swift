602dfa3e7eae57b23c36fc9bdb247b02e9769a44
Bruno <bruno.rosantos@agi.com.br>
2025-12-12 09:44:24 -0300

    Initial Commit

---
+++ HearMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HearMe
+//
+//  Created by Bruno Rodrigues dos Santos on 12/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HearMe/HearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearMeApp.swift
+//  HearMe
+//
+//  Created by Bruno Rodrigues dos Santos on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
